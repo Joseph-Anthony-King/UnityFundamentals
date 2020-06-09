c1b9bcb2781c13a4a63f4c49db5004b3486327de
Joseph-Anthony-King <Joseph.King.CA@gmail.com>
2020-06-09 00:14:35 -0700

    Complete the class

--- Assets/Scripts/Controllers/Prototypes/PlayerController.cs
+++ Assets/Scripts/Controllers/Prototypes/PlayerController.cs
@@ -3,13 +3,18 @@ using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour {
 
-    //private Animator anim;
+    private Animator anim;
     private NavMeshAgent agent;
 
 	// Use this for initialization
 	void Awake () {
 
-        //anim = GetComponent<Animator>();
+        anim = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
 	}
+
+    void Update() {
+
+        anim.SetFloat("Speed", agent.velocity.magnitude);
+    }
 }
